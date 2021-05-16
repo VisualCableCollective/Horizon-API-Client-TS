ccaf5db30360cd4442d5e200e7fae0b15cf368b0
joschiservice <joschua.hass.sh@gmail.com>
2021-05-16 11:47:45 +0200

    yey nice working eslint
    except for one error but it is working

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+    rules: {
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/lines-between-class-members': 'off',
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'airbnb-typescript',
+    ],
+  };
--- src/HorizonAPIClientConfig.ts
+++ src/HorizonAPIClientConfig.ts
@@ -1,24 +1,29 @@
 export default class HorizonAPIClientConfig {
   // Authentication
   BearerToken = '';
+
   readonly Environment: Environment;
   readonly ServerUrl: String;
 
-  constructor(env: Environment = Environment.Production, customLocalServerUrl: String = ""){
+  constructor(env: Environment = Environment.Production, customLocalServerUrl: String = '') {
     this.Environment = env;
     // Set ServerUrl
-    switch(env){
+    switch (env) {
       case Environment.LocalDevelopment:
-        this.ServerUrl = 'https://localhost:8000/';
+        if (customLocalServerUrl) {
+          this.ServerUrl = customLocalServerUrl;
+        } else {
+          this.ServerUrl = 'https://localhost:8000/';
+        }
         break;
       default:
-        this.ServerUrl = "https://api.horizon.vcc-online.eu/"
+        this.ServerUrl = 'https://api.horizon.vcc-online.eu/';
         break;
     }
   }
 }
 
-enum Environment{
+enum Environment {
   Production,
-  LocalDevelopment
+  LocalDevelopment,
 }

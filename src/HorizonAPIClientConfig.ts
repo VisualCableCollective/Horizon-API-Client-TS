a4059cac46d8bf72b877ff4405700e6fb29ab378
joschiservice <joschua.hass.sh@gmail.com>
2021-05-16 11:17:04 +0200

    removed eslint stuff and worked on config

--- src/HorizonAPIClientConfig.ts
+++ src/HorizonAPIClientConfig.ts
@@ -1,6 +1,24 @@
 export default class HorizonAPIClientConfig {
   // Authentication
   BearerToken = '';
+  readonly Environment: Environment;
+  readonly ServerUrl: String;
 
-  readonly ServerUrl = '';
+  constructor(env: Environment = Environment.Production, customLocalServerUrl: String = ""){
+    this.Environment = env;
+    // Set ServerUrl
+    switch(env){
+      case Environment.LocalDevelopment:
+        this.ServerUrl = 'https://localhost:8000/';
+        break;
+      default:
+        this.ServerUrl = "https://api.horizon.vcc-online.eu/"
+        break;
+    }
+  }
+}
+
+enum Environment{
+  Production,
+  LocalDevelopment
 }

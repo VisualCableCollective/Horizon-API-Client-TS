891db5a46cf31eac822ded39544228e5bdbdd0f4
joschiservice <joschua.hass.sh@gmail.com>
2021-05-23 11:43:22 +0200

    added GET_TEAM APIRoute

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,3 +2,4 @@
 import APIRoute, { RequestMethod } from '../models/APIRoute';
 
 export const GET_SELF_USER_DATA_ROUTE: APIRoute = { route: 'user/me', method: RequestMethod.GET, requiresAuth: true };
+export const GET_TEAM: APIRoute = { route: 'store/teams/', method: RequestMethod.GET, requiresAuth: false };

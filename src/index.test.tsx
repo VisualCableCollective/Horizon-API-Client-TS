0800f9ea94d0f1ede485b5238d6cef53b1b1ce4b
Joschua H <54814804+joschiservice@users.noreply.github.com>
2021-09-13 16:42:24 +0200

    added auth context

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import dotenv from 'dotenv';
 
-import { Environment } from './enums/Environment';
 // import Team from './models/Team';
-import { HorizonAPIClient } from './index';
-import { HorizonAPIClientConfig } from './HorizonAPIClientConfig';
+import {
+  HorizonAPIClient, HorizonAPIClientConfig, Environment,
+} from './index';
 
 dotenv.config();
 
@@ -22,22 +22,19 @@ const apiClient = new HorizonAPIClient(apiClientConfig);
   done();
 }); */
 
-test('user should authenticate with credentials', async (done) => {
-  const result = await apiClient.authenticateUserWithCredentials('joschua.hass.sh@gmail.com', 'Joschua#25770Per');
-  expect(result).toBe(true);
-  done();
-});
-
-/* if (process.env.TEST_BEARER_TOKEN) {
-  test('user should authenticate', async (done) => {
-    const result = await apiClient.authenticateUserWithToken(process.env.TEST_BEARER_TOKEN || '');
-    expect(result).toBe(true);
+if (process.env.TEST_USER_EMAIL && process.env.TEST_USER_PASSWORD) {
+  test('user should authenticate with credentials', async (done) => {
+    const result = await apiClient.authenticateUserWithCredentials(process.env.TEST_USER_EMAIL || '',
+      process.env.TEST_USER_PASSWORD || '');
+    expect(result).toBeDefined();
+    expect(result?.ok).toBe(true);
     done();
   });
 } else {
   console.warn('Skipped test for correct user authentication!');
 }
 
+/*
 // -------- TEAM TESTS --------
 // let team: Team;
 test('VCC team should be found', async (done) => {
